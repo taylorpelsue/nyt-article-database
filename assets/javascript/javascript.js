e036e0a891a9261ab34a10f618175f805846cb53
Grant Posell <grantposell@Grants-MacBook-Pro.local>
2018-03-05 21:10:06 -0600

    Added API and AJAX

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,27 @@
+//Built by Alberto, Taylor and Grant from UTCodingBootcamp Houston
 //Wait for the document to load
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $.ajax
+    //Creating an event listener
+
+    //URL to API
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += "?" + $.param({
+        'api-key' : "4e34acd533404c12a5fbdd7827988fce"
+        });
+    
+    // Performing our AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+        // After the data comes back from the API
+        .then(function (response) {
+        var results = response.data;
+            console.log(queryURL);
+    }
 
 
-    function getArticles
 
 
 
